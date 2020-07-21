d0d2c1084d0df7c3d43bce50c84948d95176c7e1
Oteri <oteri4u@gmail.com>
2020-07-21 14:21:25 +0100

    logo fixed

--- public_html/sw.js
+++ public_html/sw.js
@@ -1,5 +1,5 @@
 let appCache;
-const appCacheKey = "traderfxm.com-2020-07-21";
+const appCacheKey = "traderfxm.com-2020-07-21-01";
 
 self.addEventListener("push", (event) => {
   const { subject, body } = event.data.json();
