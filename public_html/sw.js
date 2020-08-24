35616784cddd12cb60123cd391e379dee558bfb7
Oteri <oteri4u@gmail.com>
2020-08-24 07:26:59 +0100

    service worker updated

--- public_html/sw.js
+++ public_html/sw.js
@@ -1,5 +1,5 @@
 let appCache;
-const appCacheKey = "traderfxm.com-2020-08-14";
+const appCacheKey = "traderfxm.com-2020-08-24";
 
 self.addEventListener("push", (event) => {
   const { subject, body } = event.data.json();
