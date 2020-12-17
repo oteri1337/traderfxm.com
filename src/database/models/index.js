99c77d0bb2c1920a0e64ba2acb0c8cbc7e7995b3
Oteri <oteri4u@gmail.com>
2020-12-17 15:39:09 +0100

    database fixed

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,53 +1,53 @@
-// "use strict";
+"use strict";
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// const fs = require("fs");
-// const path = require("path");
-// const Sequelize = require("sequelize");
-// const basename = path.basename(__filename);
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
 
-// console.log("env", process.env.NODE_ENV);
+console.log("env", process.env.NODE_ENV);
 
-// const env = process.env.NODE_ENV || "development";
-// let config = require(__dirname + "/../../../database.json")[env];
+const env = process.env.NODE_ENV || "development";
+let config = require(__dirname + "/../../../database.json")[env];
 
-// const db = {};
+const db = {};
 
-// let sequelize;
+let sequelize;
 
-// const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_TYPE } = process.env;
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_TYPE } = process.env;
 
-// config.host = DB_HOST;
+config.host = DB_HOST;
 
-// config.dialect = DB_TYPE;
+config.dialect = DB_TYPE;
 
-// config.operatorsAliases = 0;
+config.operatorsAliases = 0;
 
-// sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, config);
+sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, config);
 
-// fs.readdirSync(__dirname)
-//   .filter((file) => {
-//     return (
-//       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-//     );
-//   })
-//   .forEach((file) => {
-//     // const model = sequelize["import"](path.join(__dirname, file));
-//     const model = require(path.join(__dirname, file))(
-//       sequelize,
-//       Sequelize.DataTypes
-//     );
-//     db[model.name] = model;
-//   });
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    // const model = sequelize["import"](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
 
-// Object.keys(db).forEach((modelName) => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// module.exports = db;
+module.exports = db;
